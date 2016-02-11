62131088e31b56ed2ac68e5d2aa7b33a17e3f703
darapenhchet <darapenhchet@gmail.com>
2016-02-11 08:37:11 +0700

    RRRR

--- WeBillIntegration/src/main/java/kh/com/kshrd/repositories/BillRepository.java
+++ WeBillIntegration/src/main/java/kh/com/kshrd/repositories/BillRepository.java
@@ -1,10 +1,7 @@
 package kh.com.kshrd.repositories;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import kh.com.kshrd.entities.Account;
 import kh.com.kshrd.entities.Bill;
 
 public interface BillRepository extends PagingAndSortingRepository<Bill, Long> {
